40459e53a8725bbcec8ef757647a17c19318167b
Yash Parmar <yashparmar2772004@gmail.com>
2024-07-31 10:57:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import FeatureSection from "./components/FeatureSection"
+import Footer from "./components/Footer"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import Price from "./components/Price"
+import Testimonials from "./components/Testimonials"
+import WorkFlow from "./components/WorkFlow"
 
 const App = () => {
   return (
@@ -8,13 +12,29 @@ const App = () => {
    {/* Navbar */}
    <Navbar/>
 
-   {/* Hero-Section */}
+  
    <div className="max-w-7xl mx-auto pt-20 px-6">
-   <HeroSection />
-   </div>
 
+     {/* Hero-Section */}
+   <HeroSection />
+   
    {/* Feature-Section */}
    <FeatureSection />
+
+   {/* Work-Flow */}
+   <WorkFlow/>
+
+   {/* Price */}
+   <Price/>
+
+   {/* testimonials */}
+   <Testimonials />
+
+   {/* Footer */}
+   <Footer />
+
+   </div>
+
    </>
   )
 }
